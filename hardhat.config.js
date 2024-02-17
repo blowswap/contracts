3380451b9a03664018eca4c4e2f4c814efea5b3b
blowswap_dev <blowswap@protonmail.com>
2024-02-17 22:29:48 +0800

    add token and weth9

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,8 @@ const {blast_sepolia_private_key} = process.env;
 module.exports = {
   solidity:{
     compilers:[
+      {version: "0.5.16", settings: {optimizer: {enabled: true, runs: 200}}},
+      {version: "0.6.6", settings: {optimizer: {enabled: true, runs: 200}}},
       {version: "0.6.12", settings: {optimizer: {enabled: true, runs: 200}}},
       {version: "0.8.12", settings: {optimizer: {enabled: true, runs: 200}}},
       {version: "0.8.18", settings: {optimizer: {enabled: true, runs: 200}}}
